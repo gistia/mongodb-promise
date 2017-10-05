e160a3c0add3481e41da67a3a35bb6afdef6b22f
Felipe Coury <felipe.coury@gmail.com>
2017-10-05 16:32:11 -0300

    wip: close connections

--- src/client.js
+++ src/client.js
@@ -8,13 +8,29 @@ class Client {
     this.url = url || process.env.MONGODB_URL;
   }
 
+  get connectionOptions() {
+    const getInt = (v, defVal) => {
+      const intVal = parseInt(v, 10);
+      if (isNaN(intVal)) {
+        return defVal;
+      }
+      return intVal;
+    }
+
+    return {
+      poolSize: getInt(process.env.MONGODB_POOL_SIZE, 10),
+    };
+  }
+
   connect() {
     if (this.connection) {
       return Promise.resolve(this.connection);
     }
 
+    console.log(' *** options', this.connectionOptions);
+
     return new Promise((resolve, reject) => {
-      client.connect(this.url, (err, db) => {
+      client.connect(this.url, this.connectionOptions, (err, db) => {
         if (err) {
           reject(err);
           return;
@@ -30,6 +46,12 @@ class Client {
     return new Promise((resolve, reject) => {
       this.connect().then(db => resolve(db.collection(name)), reject).catch(reject);
     });
+  }
+
+  close() {
+    const conn = this.connection;
+    this.connection = null;
+    conn.close();
   }
 
   query(collection) {
