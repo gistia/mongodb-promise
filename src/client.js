bc0efdf7fa32086a4e932d683ae77ef38a77807e
Everton Cardoso <verto@verto.me>
2018-05-09 18:02:00 -0300

    feat: adding findAndModify support

--- src/client.js
+++ src/client.js
@@ -92,6 +92,19 @@ class Client {
     });
   }
 
+  findAndModify(collectionName, query, sort={}, doc, options={}) {
+    return new Promise((resolve, reject) => {
+      this.withCollection(collectionName).then(({ conn, collection }) => {
+        collection.findAndModify(query, sort, doc, options,
+          (err, result) => {
+            if (err) { return reject(err); }
+            resolve(result);
+          }
+        );
+      }, reject).catch(reject);
+    });
+  }
+
   insert(collectionName, doc) {
     return new Promise((resolve, reject) => {
       this.withCollection(collectionName).then(({ conn, collection }) => {
